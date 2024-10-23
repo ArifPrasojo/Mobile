7a09b1d32ac80f5724eaf248cbbc84caa541ae52
username Anda <arifprasojo999@email.com>
2024-10-24 02:00:55 +0700

    first commit

---
+++ p6_layoutnavigasi/android/app/src/main/kotlin/com/example/p6_layoutnavigasi/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.p6_layoutnavigasi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ p6_layoutnavigasi/lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    // Widget titleSection yang berisi struktur Row dan Column untuk title
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32), // padding 32 untuk seluruh Row
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start, // Column di-align ke awal baris
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8), // padding 8 untuk teks pertama
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(
+                    color: Colors.grey, // warna abu-abu untuk teks kedua
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Icon(
+            Icons.star,
+            color: Colors.red, // ikon bintang berwarna merah
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
+    // Membuat bagian tombol
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+    // Widget textSection yang berisi teks tambahan
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Carilah teks di internet yang sesuai '
+        'dengan foto atau tempat wisata yang ingin '
+        'Anda tampilkan. '
+        'Tambahkan nama dan NIM Anda sebagai '
+        'identitas hasil pekerjaan Anda. '
+        'Selamat mengerjakan 🙂.',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Wisata Gunung di Batu'),
+        ),
+        body: ListView(
+          children: [
+            titleSection,  // Title section
+            buttonSection, // Button section
+            textSection,   // Text section
+          ],
+        ),
+      ),
+    );
+  }
+
+  // Metode untuk membangun kolom tombol dengan ikon dan teks
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
